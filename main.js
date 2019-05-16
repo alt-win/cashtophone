c8a5a9a7ee6b33ac62acb4d3443e00e23de72690
Alterntive_Winter <38795952+alt-win@users.noreply.github.com>
2019-05-16 14:19:44 -0700

    Open_in_wallet

--- main.js
+++ main.js
@@ -4,36 +4,40 @@ function displayPaymentInfo() {
     var errorField = document.getElementById("error");
     var QRCodeImage = document.getElementById("QRCode");
     var scanMessage = document.getElementById("scanMessage");
+    var openWallet = document.getElementById("openInWallet");
+
     phoneNumber = phoneNumber.trimLeft("+");
-    
+
     //Check that both fields are populated.
     if (!(bchAmount && phoneNumber)){
         errorField.innerHTML = "Please fill in both fields.";
         setTimeout(function(){document.getElementById("error").innerHTML="";}, 5000);
     }
-    
+
     else if (isNaN(phoneNumber)){
         errorField.innerHTML = "Please only enter numbers into telephone number field.";
         setTimeout(function(){document.getElementById("error").innerHTML="";}, 5000);
     }
-    
+
     else if (isNaN(bchAmount)){
         errorField.innerHTML = "Please only enter number into BCH amount field.";
         setTimeout(function(){document.getElementById("error").innerHTML="";}, 5000);
     }
-    
+
     else {
         var errorFree = true; //to remove need to code rest of the function indented under this else {}
     }
-    
-    if (!errorFree){ 
+
+    if (!errorFree){
         return; //the following code will only run if no errors above were detected
     }
-    
+
     var url = "https://pay.cointext.io/p/" + phoneNumber + "/" + (bchAmount*100000000).toString();
     console.log(url);
-    
+
     QRCodeImage.src = url;
     QRCodeImage.style.display = "block";
     scanMessage.style.display = "block";
+    openWallet.setAttribute('href', "bitcoincash:?r="+url+"");
+    openWallet.style.display = "block";
 }
